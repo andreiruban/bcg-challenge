c94e3bebc17cbc82cec1ec6962c329f8dac9b433
andreiruban <herr.ruban@yandex.com>
2020-07-09 01:04:16 +0200

    HttpController.kt rework

--- src/main/kotlin/io/ruban/bcgchallenge/api/HttpController.kt
+++ src/main/kotlin/io/ruban/bcgchallenge/api/HttpController.kt
@@ -3,6 +3,7 @@ package io.ruban.bcgchallenge.api
 import io.ruban.bcgchallenge.domain.model.RatedSongView
 import io.ruban.bcgchallenge.domain.service.DataAggregator
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 
@@ -11,12 +12,8 @@ class HttpController(
         private val dataAggregator: DataAggregator
 ) {
 
-    @GetMapping("/")
-    fun hello() = "Hello!"
-
-    @GetMapping("/songs/charts")
-//    fun getChart(@PathVariable("top") top: Int): List<RatedSongView> {
-    fun getChart(): List<RatedSongView> {
-        return dataAggregator.generateChart(5)
+    @GetMapping("/songs/top/{top}")
+    fun getChart(@PathVariable("top") top: Int): List<RatedSongView> {
+        return dataAggregator.generateChart(top)
     }
 }
