8c88658152cbf0d49c0ebe6aa2ea9e76b0aa36ec
andreiruban <herr.ruban@yandex.com>
2020-07-10 01:16:50 +0200

    API has been reworked

--- src/main/kotlin/io/ruban/bcgchallenge/api/SongChartRestResource.kt
+++ src/main/kotlin/io/ruban/bcgchallenge/api/SongChartRestResource.kt
@@ -2,7 +2,7 @@ package io.ruban.bcgchallenge.api
 
 import io.ruban.bcgchallenge.domain.service.DataAggregator
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.time.OffsetDateTime
 
@@ -12,8 +12,8 @@ class SongChartRestResource(
         private val dataAggregator: DataAggregator
 ) {
 
-    @GetMapping("/songs/charts?top={top}")
-    fun getChart(@PathVariable("top") top: Int): ChartContainer {
+    @GetMapping("/songs/charts")
+    fun getChart(@RequestParam("top") top: Int): ChartContainer {
         val chart = dataAggregator.generateChart(top)
         return ChartContainer(chart = chart, generatedAt = OffsetDateTime.now())
     }
