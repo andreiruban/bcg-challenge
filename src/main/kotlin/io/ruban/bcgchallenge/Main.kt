bc2a100b5ca173ae665d0a7fbfd19bb9d0d3cbc4
andreiruban <herr.ruban@yandex.com>
2020-07-09 23:45:17 +0200

    Framework config has been extended, test profile has been added

--- src/main/kotlin/io/ruban/bcgchallenge/Main.kt
+++ src/main/kotlin/io/ruban/bcgchallenge/Main.kt
@@ -2,10 +2,8 @@ package io.ruban.bcgchallenge
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
-@EnableScheduling
 class BcgChallengeApplication
 
 fun main(args: Array<String>) {
---
+++ src/main/kotlin/io/ruban/bcgchallenge/config/SpringBootConfiguration.kt
@@ -0,0 +1,10 @@
+package io.ruban.bcgchallenge.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import org.springframework.scheduling.annotation.EnableScheduling
+
+@Profile("!test")
+@Configuration
+@EnableScheduling
+class SpringBootConfiguration
